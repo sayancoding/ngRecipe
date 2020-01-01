3a6f0b52a9de6ba6720b224ef4b24ede85e0313a
SAYAN MAITY <sayanmaity007@gmail.com>
2020-01-01 12:55:51 +0530

    adding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, Input } from '@angular/core';
-import {FormsModule} from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ServersComponent } from "../components/servers/server.component";
-import { SubServerComponent } from './sub-server/sub-server.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServersComponent,
-    SubServerComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
