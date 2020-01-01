0aa8109625f409b2fd7c676fe0210fb44f97c661
SAYAN MAITY <sayanmaity007@gmail.com>
2020-01-01 13:08:45 +0530

    struc comp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,22 @@ import { NgModule, Input } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { ShopingListComponent } from './shoping-list/shoping-list.component';
+import { ShopingEditComponent } from './shoping-list/shoping-edit/shoping-edit.component';
+import { RecipeComponent } from './recipe/recipe.component';
+import { RecipeItemComponent } from './recipe/recipe-item/recipe-item.component';
+import { RecipeListComponent } from './recipe/recipe-list/recipe-list.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    ShopingListComponent,
+    ShopingEditComponent,
+    RecipeComponent,
+    RecipeItemComponent,
+    RecipeListComponent,
   ],
   imports: [
     BrowserModule,
